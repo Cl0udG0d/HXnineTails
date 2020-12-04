3fb5c8efab72da79dea86bee4a8d4f114a8e367d
潘一 <2585614464@qq.com>
2020-12-04 11:28:10 +0800

    edit pppXray

--- Xray/pppXray.py
+++ Xray/pppXray.py
@@ -26,7 +26,7 @@ def logo():
 
 
 def xrayScan(targeturl,outputfilename="test"):
-    scanCommand="{} webscan --basic-crawler {} --html-output {}\{}.html".format(config.Xray_Path,targeturl,config.Xray_report_path,outputfilename)
+    scanCommand="{} webscan --url \"{}\" --html-output {}\{}.html".format(config.Xray_Path,targeturl,config.Xray_report_path,outputfilename)
     print(scanCommand)
     os.system(scanCommand)
     return
